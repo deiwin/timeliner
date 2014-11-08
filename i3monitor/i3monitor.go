0ef3e0078e35578a4be6459fb3d8376bd2ad0ab2
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-11-08 20:11:27 +0200

    update i3monitor to receive throttled workspace change events

--- i3monitor/i3monitor.go
+++ i3monitor/i3monitor.go
@@ -1,14 +1,40 @@
 package i3monitor
 
-// import "github.com/proxypoke/i3ipc"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/deiwin/timeliner/throttle"
+	"github.com/proxypoke/i3ipc"
+)
 
 type ActiveWindowUpdate struct {
 	Workspace string
 	Window    string
 }
 
-func SubscribeToActiveWindowUpdates() (subs chan ActiveWindowUpdate) {
-	subs = make(chan ActiveWindowUpdate)
+const throttleDuration = 1 * time.Second
+
+func SubscribeToActiveWindowUpdates() (out chan ActiveWindowUpdate) {
+	out = make(chan ActiveWindowUpdate)
+
+	workspaceEvents, err := i3ipc.Subscribe(i3ipc.I3WorkspaceEvent)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	throttledWorkspaceEvents := throttle.Throttle(workspaceEvents, throttleDuration)
+
+infninteLoop:
+	for {
+		select {
+		case event := <-throttledWorkspaceEvents:
+			fmt.Printf("Received an event: %#v\n", event)
+		case <-time.After(5 * time.Second):
+			break infninteLoop
+		}
+	}
 
 	return
 }
