d6917fd29295d710b7534494e2a59dadca87ab2a
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-11-08 18:15:26 +0200

    refactor main functionality into a separate method
    I think it looks better this way

--- throttle.go
+++ throttle.go
@@ -6,35 +6,35 @@ type Throttleable interface{}
 
 func Throttle(in <-chan int, duration time.Duration) (out chan int) {
 	out = make(chan int)
+	go throttleSynchronously(in, out, duration)
+	return
+}
 
-	go func() {
-		var (
-			lastMessage    int
-			messagePending bool
-			ok             bool
-		)
+func throttleSynchronously(in <-chan int, out chan int, duration time.Duration) {
+	var (
+		lastMessage    int
+		messagePending bool
+		ok             bool
+	)
 
-		for {
-			if messagePending {
-				select {
-				case lastMessage = <-in:
+	for {
+		if messagePending {
+			select {
+			case lastMessage = <-in:
+				messagePending = true
+			case <-time.After(duration):
+				out <- lastMessage
+				messagePending = false
+			}
+		} else {
+			select {
+			case lastMessage, ok = <-in:
+				if ok {
 					messagePending = true
-				case <-time.After(duration):
-					out <- lastMessage
-					messagePending = false
-				}
-			} else {
-				select {
-				case lastMessage, ok = <-in:
-					if ok {
-						messagePending = true
-					} else {
-						break
-					}
+				} else {
+					break
 				}
 			}
 		}
-	}()
-
-	return
+	}
 }
