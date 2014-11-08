0ddf336c1c7bfe5baf14d2da90dbe58cdc2dfdf3
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-11-08 17:24:08 +0200

    implement initial working version

--- throttle.go
+++ throttle.go
@@ -1,8 +1,36 @@
 package throttle
 
+import "time"
+
 type Throttleable interface{}
 
-func Throttle(in <-chan int32) (out <-chan int32) {
+func Throttle(in <-chan int, duration time.Duration) (out chan int) {
+	out = make(chan int)
+
+	go func() {
+		var (
+			lastMessage    int
+			messagePending bool
+		)
+
+		for {
+			if messagePending {
+				select {
+				case lastMessage = <-in:
+					messagePending = true
+				case <-time.After(duration):
+					out <- lastMessage
+					messagePending = false
+				}
+			} else {
+				select {
+				case lastMessage = <-in:
+					messagePending = true
+				}
+				// could also check every once in a while if the in channel is closed to break out
+			}
+		}
+	}()
 
 	return
 }
--- throttle_test.go
+++ throttle_test.go
@@ -5,8 +5,55 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"time"
 )
 
 var _ = Describe("Throttle", func() {
+	var channel chan int
 
+	BeforeEach(func() {
+		channel = make(chan int)
+	})
+
+	It("should return", func() {
+		Expect(Throttle(channel, time.Nanosecond)).NotTo(BeNil())
+	})
+
+	Context("with a channel throttled by a microsecond", func() {
+		var throttled chan int
+
+		BeforeEach(func() {
+			throttled = Throttle(channel, time.Microsecond)
+		})
+
+		It("should return the only element pushed", func(done Done) {
+			defer close(done)
+
+			channel <- 1
+			Expect(<-throttled).To(Equal(1))
+		})
+
+		It("should return the last of two consecutively pushed elements", func(done Done) {
+			defer close(done)
+
+			channel <- 1
+			channel <- 2
+			Expect(<-throttled).To(Equal(2))
+		})
+
+		It("should return the last of two consecutively pushed elements, twice", func(done Done) {
+			defer close(done)
+
+			channel <- 1
+			channel <- 2
+			Expect(<-throttled).To(Equal(2))
+
+			time.Sleep(2 * time.Microsecond)
+
+			channel <- 3
+			channel <- 4
+			Expect(<-throttled).To(Equal(4))
+		})
+	})
 })
